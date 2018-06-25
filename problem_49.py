393241be969c4300bfcb46514bf2a49796cb56d7
matt <matt@shapeways.com>
2018-06-25 13:51:01 -0400

    initial

---
+++ problem_49.py
@@ -0,0 +1,19 @@
+import math
+
+def gen_primes(start, limit):
+    primes = []
+    for i in range(start, limit):
+        is_prime = True
+        for x in range(2, int(math.sqrt(i))):
+            if i % x == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
+
+    return primes
+
+start = 1000
+limit = 10000
+
+primes = gen_primes(start, limit)
